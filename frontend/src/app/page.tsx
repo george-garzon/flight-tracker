297c0e7dd9055faac9bab30f567d26b55b0b601f
George Garzon Mac <george.garzon@olympiapharmacy.com>
2025-07-22 23:19:45 -0400

    Flight Tracker (Monorepo) Flask + Nextjs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -26,10 +26,12 @@ export default async function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <main>
-          <h1>Flight Tracker</h1>
-          <p>{message}</p>
+            <h1 className="text-5xl font-bold text-center light:text-black dark:text-white">
+              Flight Tracker
+            </h1>
+            <p>{message}</p>
         </main>
-          <DataTableDemo />
+        <DataTableDemo/>
       </main>
     </div>
   );
